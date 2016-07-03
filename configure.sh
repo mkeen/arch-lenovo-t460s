1e21d6aef304fd42972deca3ebb4348bade10647
Mike Keen <mkeen@getluma.com>
2016-07-03 00:51:00 -0400

    Minor fix, also split up setup process for context of arch-chroot

---
+++ configure.sh
@@ -0,0 +1,61 @@
+# Locale
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+
+# Time
+tzselect
+ln -s /usr/share/zoneinfo/America/New_York /etc/localtime
+hwclock --systohc --utc
+
+# Configure Boot
+pacman -S intel-ucode --noconfirm
+cp skel/etc/mkinitcpio.conf /etc/mkinitcpio.conf
+mkinitcpio -p linux
+efibootmgr -d /dev/${DISK} -p 1 -c -L "Arch Linux" -l /vmlinuz-linux -u "i915.preliminary_hw_support=1 root=/dev/${DISK}5 rw initrd=/intel-ucode.img initrd=/initramfs-linux.img"
+
+# Create User
+useradd mkeen --create-home --password \$1\$o4fUysim\$.ije9dcXJAbmiU.M3OhPg1
+cp sudoers /etc/sudoers
+chmod 0440 /etc/sudoers
+chown root /etc/sudoers
+
+# Set Hostname
+echo "resin" > /etc/hostname
+
+# Install Base Customizations
+echo "
+[archlinuxfr]
+SigLevel = Never
+Server = http://repo.archlinux.fr/\$arch
+" >> /etc/pacman.conf
+pacman -Syy --noconfirm
+pacman -S yaourt --noconfirm
+
+yaourt --noconfirm -S pam_mount gnome emacs nvm-git git wget unzip
+
+# Configure Erlang Version Manager
+git clone https://github.com/robisonsantos/evm.git
+cd evm
+./install
+cd ../
+rm -rf evm
+echo "source $HOME/.evm/scripts/evm" > ~/.bashrc
+exec $SHELL
+evm install OTP_18.3
+evm default OTP_18.3
+
+# Configure Elixir Version Manager
+git clone git://github.com/mururu/exenv.git ~/.exenv
+echo 'export PATH="$HOME/.exenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(exenv init -)"' >> ~/.bashrc
+exec $SHELL
+wget https://github.com/elixir-lang/elixir/archive/v1.3.1.zip
+unzip v1.3.1.zip
+mv elixir-1.3.1/ ~/.exenv/versions/1.3.1
+exenv global 1.3.1
+exenv rehash
+
+systemctl enable gdm
+exit
+reboot
--- setup.sh
+++ setup.sh
@@ -59,73 +59,12 @@ pacstrap /mnt base base-devel
 genfstab -U /mnt >> /mnt/etc/fstab
 
 # Skel files
-cp skel/etc/unsecure.key /etc
-cp skel/etc/crypttab /etc
-cp skel/etc/security/pam_mount.conf.xml /etc/security
-cp skel/etc/pam.d/gdm-password /etc/pam.d
-cp skel/etc/pam.d/system-auth /etc/pam.d
-# Probably needs permissions fixes
+cp skel/etc/unsecure.key /mnt/etc
+cp skel/etc/crypttab /mnt/etc
+cp skel/etc/security/pam_mount.conf.xml /mnt/etc/security
+cp skel/etc/pam.d/gdm-password /mnt/etc/pam.d
+cp skel/etc/pam.d/system-auth /mnt/etc/pam.d
+cp configure.sh /mnt/tmp
+# Probably needs permissions fixes inside of configure.sh's first lines
 
-arch-chroot /mnt /bin/bash
-
-# Locale
-echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
-locale-gen
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
-
-# Time
-tzselect
-ln -s /usr/share/zoneinfo/America/New_York /etc/localtime
-hwclock --systohc --utc
-
-# Configure Boot
-pacman -S intel-ucode --noconfirm
-cp skel/etc/mkinitcpio.conf /etc/mkinitcpio.conf
-mkinitcpio -p linux
-efibootmgr -d /dev/${DISK} -p 1 -c -L "Arch Linux" -l /vmlinuz-linux -u "i915.preliminary_hw_support=1 root=/dev/${DISK}5 rw initrd=/intel-ucode.img initrd=/initramfs-linux.img"
-
-# Create User
-useradd mkeen --create-home --password \$1\$o4fUysim\$.ije9dcXJAbmiU.M3OhPg1
-cp sudoers /etc/sudoers
-chmod 0440 /etc/sudoers
-chown root /etc/sudoers
-
-# Set Hostname
-echo "resin" > /etc/hostname
-
-# Install Base Customizations
-echo "
-[archlinuxfr]
-SigLevel = Never
-Server = http://repo.archlinux.fr/\$arch
-" >> /etc/pacman.conf
-pacman -Syy --noconfirm
-pacman -S yaourt --noconfirm
-
-yaourt --noconfirm -S pam_mount gnome emacs nvm-git git wget unzip
-
-# Configure Erlang Version Manager
-git clone https://github.com/robisonsantos/evm.git
-cd evm
-./install
-cd ../
-rm -rf evm
-echo "source $HOME/.evm/scripts/evm" > ~/.bashrc
-exec $SHELL
-evm install OTP_18.3
-evm default OTP_18.3
-
-# Configure Elixir Version Manager
-git clone git://github.com/mururu/exenv.git ~/.exenv
-echo 'export PATH="$HOME/.exenv/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(exenv init -)"' >> ~/.bashrc
-exec $SHELL
-wget https://github.com/elixir-lang/elixir/archive/v1.3.1.zip
-unzip v1.3.1.zip
-mv elixir-1.3.1/ ~/.exenv/versions/1.3.1
-exenv global 1.3.1
-exenv rehash
-
-systemctl enable gdm
-exit
-reboot
+arch-chroot /mnt /bin/bash "sh /tmp/configure.sh"
