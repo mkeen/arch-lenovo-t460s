3e8b01c0d005ec91da123bf33d5ebc1abaa25740
Mike Keen <mkeen@getluma.com>
2016-07-03 00:19:33 -0400

    real world fixes

--- setup.sh
+++ setup.sh
@@ -7,36 +7,36 @@ pacman -Syy
 pacman --noconfirm -S cryptsetup
 
 # Prepare Partitions
-parted /dev/${DISK} --script mklabel msdos
+parted /dev/${DISK} --script mklabel gpt
 
 # Boot Partition
 parted /dev/${DISK} --script mkpart ESP fat32 1MiB 513MiB
-parted /dev/${DISK} --script disk_set boot on
+parted /dev/${DISK} --script set 1 boot on
 mkfs.fat -F32 /dev/${DISK}1
 
 # Encrypted Swap Partition
 parted /dev/${DISK} --script mkpart primary linux-swap 513MiB 10000MiB
-cryptsetup open --type plain /dev/${DISK}2 container --key-file /dev/random
-mkswap /dev/mapper/${DISK}2
-swapon /dev/mapper/${DISK}2
+cryptsetup open --type plain /dev/${DISK}2 swap --key-file /dev/random
+mkswap /dev/mapper/swap
+swapon /dev/mapper/swap
 
 # Encrypted Tmp Partition
 parted /dev/${DISK} --script mkpart primary ext4 10001MiB 20000MiB
-cryptsetup open --type plain /dev/${DISK}3 container --key-file /dev/random
-mkfx.ext4 /dev/mapper/${DISK}3
+cryptsetup open --type plain /dev/${DISK}3 tmp --key-file /dev/random
+mkfx.ext4 /dev/mapper/tmp
 
 # Encrypted Var Partition
 parted /dev/${DISK} --script mkpart primary ext4 20001MiB 50000MiB
-cryptsetup open /dev/${DISK}4
-mkfs.ext4 /dev/mapper/${DISK}4
+cryptsetup open --type plain /dev/${DISK}4 var
+mkfs.ext4 /dev/mapper/var
 
 # / Partition
 parted /dev/${DISK} --script mkpart primary ext4 50001MiB 80000MiB
 mkfs.ext4 /dev/${DISK}5
 
-# Home Folder (/home/mkeen) Partition
+# Encrypted Home Folder (/home/mkeen) Partition
 parted /dev/${DISK} --script mkpart primary ext4 80001MiB 100%
-cryptsetup open /dev/${DISK}6
+cryptsetup open --type plain /dev/${DISK}6 home
 mkfs.ext4 /dev/mapper/${DISK}6
 
 # Mount All
@@ -55,13 +55,16 @@ reflector --verbose --country 'United States' -l 200 -p http --sort rate --save 
 # Pacstrap
 pacstrap /mnt base base-devel
 genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt /bin/bash
 
+# Skel files
+cp skel/etc/unsecure.key /etc
 cp skel/etc/crypttab /etc
 cp skel/etc/security/pam_mount.conf.xml /etc/security
 cp skel/etc/pam.d/gdm-password /etc/pam.d
 cp skel/etc/pam.d/system-auth /etc/pam.d
 # Probably needs permissions fixes
+
+arch-chroot /mnt /bin/bash
 
 # Locale
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
@@ -76,9 +79,8 @@ hwclock --systohc --utc
 # Configure Boot
 pacman -S intel-ucode --noconfirm
 cp skel/etc/mkinitcpio.conf /etc/mkinitcpio.conf
-efibootmgr -d /dev/${DISK} -p 1 -c -L "Arch Linux" -l /vmlinuz-linux -u "i915.preliminary_hw_support=1 root=/dev/${DISK}5 rw initrd=/intel-ucode.img initrd=/initramfs-linux.img"
 mkinitcpio -p linux
-efibootmgr -v
+efibootmgr -d /dev/${DISK} -p 1 -c -L "Arch Linux" -l /vmlinuz-linux -u "i915.preliminary_hw_support=1 root=/dev/${DISK}5 rw initrd=/intel-ucode.img initrd=/initramfs-linux.img"
 
 # Create User
 useradd mkeen --create-home --password \$1\$o4fUysim\$.ije9dcXJAbmiU.M3OhPg1
